d3e4cdd556c2527efe57ca1d377e87162a6fa698
Daniel Compton <desk@danielcompton.net>
2018-03-24 21:19:34 +1300

    0.5.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/tracing "0.5.0"
+(defproject day8.re-frame/tracing "0.5.1-SNAPSHOT"
   :description "A tool for inspecting code execution for re-frame applications"
   :url "https://github.com/philoskim/debux"
   :license {"Eclipse Public License"
