cb3a7d289d14c716ae39afca959fcc2a12431323
Daniel Compton <desk@danielcompton.net>
2018-04-05 00:35:59 +1200

    Add tracing-stubs deploy repositories

--- tracing-stubs/project.clj
+++ tracing-stubs/project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/Day8/re-frame-debux"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories {"releases" :clojars
+                        "snapshots" :clojars}
   :dependencies [[org.clojure/clojure "1.8.0"]])
