3b8cf96c3bbccd6b6a364d1b5ea5cbe86aa173bf
Isaac Johnston <isaac@superstruct.nz>
2019-06-24 01:00:36 +0300

    Upgrade ClojureScript to 1.10.520

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {"Eclipse Public License"
             "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.854" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [clojure-future-spec "1.9.0-alpha17"]
                  [re-frame "0.10.4" :scope "provided"]]
 
