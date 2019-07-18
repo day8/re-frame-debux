18d08f6e76bdac909ff4e65f2b16b6f7af046bce
Isaac Johnston <isaac@superstruct.nz>
2019-07-18 09:43:11 +0800

    Version 0.5.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/tracing "0.5.3"
+(defproject day8.re-frame/tracing "0.5.4-SNAPSHOT"
   :description "A tool for inspecting code execution for re-frame applications"
   :url "https://github.com/philoskim/debux"
   :license {"Eclipse Public License"
