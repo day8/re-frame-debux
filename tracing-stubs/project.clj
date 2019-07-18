484346612ddf7c1d5a0239921a97a5008a2e2693
Isaac Johnston <isaac@superstruct.nz>
2019-07-18 09:48:56 +0800

    Add tracing-stubs release-tasks

--- tracing-stubs/project.clj
+++ tracing-stubs/project.clj
@@ -3,8 +3,20 @@
   :url "https://github.com/Day8/re-frame-debux"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
                                     :username :env/CLOJARS_USERNAME
                                     :password :env/CLOJARS_PASSWORD}]]
+  
+  :scm {:dir ".."}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]])
