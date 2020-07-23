0b13a2dd6cc2c8003b167a2022740b30d2bde50a
Isaac Johnston <isaac@superstruct.nz>
2020-07-24 11:51:15 +1200

    Upgrade ClojureScript to 1.10.773

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                 "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure       "1.10.1" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.764" :scope "provided"
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
