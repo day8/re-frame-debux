ee486335e23dc740efd7c275e4caf868fd32098a
Isaac Johnston <isaac@superstruct.nz>
2020-08-26 14:41:01 +1200

    Upgrade shadow-cljs to 2.11.0

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs      "2.10.17" :scope "provided"]
+                 [thheller/shadow-cljs      "2.11.0" :scope "provided"]
                  [clojure-future-spec       "1.9.0"]
                  [re-frame                  "1.0.0" :scope "provided"]
                  [net.cgrand/macrovich      "0.2.1"]]
